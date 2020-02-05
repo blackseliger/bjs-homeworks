f5d2cd78b36550490c34011585861a60de054428
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-02-05 20:35:18 +0300

    2 попытка 1 задачи

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,34 +1,25 @@
 function getResult(a,b,c){
     "use strict"
-    // let a;
-// let b;
-// let c;      Что значит реализовать функцию? Такой же вопрос на 51 строке.
-// d = b**2 - 4*a*c 
-let result = [];
-Math.pow(b, 2);
-let d = b - 4*a*c;
-if ( d < 0) {
-    result = [];
-} else if ( d > 0 ) {
-    let x1, x2;
-    Math.sqrt(d);
-    x1 = ( (-b) + d )/(2*a);
-    x2 = ( (b) + d )/(2*a);
-    result.push(x1 , x2);
-    return result[x1][x2];
-} else if ( d = 0) {
-    let x1;
-    Math.sqrt(d);
-    x1 = ( (-b) + d )/(2*a);
-    result.push(x1);
-    return result[x1];
-} 
-// return x1, x2;
+    let discriminate; 
+    discriminate = b**2 - 4*a*c;
+    if ( discriminate = 0) {
+        let result = [x1];
+        x1 = ( (-b))/(2*a);
+        return result;
+    } else if (discriminate < 0) {
+        let result = []
+        return result;
+    } else if ( discriminate > 0) {
+        let result = [x1, x2];
+        x1 = ( (-b) + Math.sqrt(d))/(2*a);
+        x2 = ( (-b) - Math.sqrt(d))/(2*a);
+        return result;
+    }
     // код для задачи №1 писать здесь
     //return x;
 }
     // return x;
-}
+
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
