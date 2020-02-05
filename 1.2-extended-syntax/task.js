551abb20b7fa2c15bbe11c226c4b6e5c259ff73d
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-02-05 23:46:48 +0300

    3 работа

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -22,8 +22,24 @@ function getResult(a,b,c){
 
 function getAverageMark(marks){
     
-    
-    // return averageMark;
+    if (marks.length > 5) {
+            let sum = 0;
+            console.log("Превышено количество оценок");
+            marks.splice(5, marks.length);
+            for ( let i = 0; i < marks.length; i++) {
+                sum = sum + marks[i];
+            } 
+            let average = sum/marks.length;
+            return average;
+        } else if (marks.length <= 5) {
+            let sum = 0;
+            for ( let i = 0; i < marks.length; i++) {
+                sum = sum + marks[i];     //  Почему, если тут написать sum =+ marks[i]; То команда не будет работать? 
+                
+            } 
+            let average = sum/marks.length;
+            return average; 
+        }
 }
 
 function askDrink(name,dateOfBirthday){
