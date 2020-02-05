d513343fbb391cb2ed2e3214e700a074433ee80b
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-02-05 21:31:57 +0300

    1 задание сделано

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,18 +3,18 @@ function getResult(a,b,c){
     let discriminate; 
     discriminate = b**2 - 4*a*c;
     if ( discriminate == 0) {
-        // let result = [x1];
         let result = [];
-        x1 = ( (-b))/(2*a);
+        let x1 = ( (-b))/(2*a);
         result.push(x1);
         return result;
     } else if (discriminate < 0) {
         let result = [];
         return result;
     } else if ( discriminate > 0) {
-        let result = [x1, x2];
-        x1 = ( (-b) + Math.sqrt(d))/(2*a);
-        x2 = ( (-b) - Math.sqrt(d))/(2*a);
+        let result = [];
+        let x1 = ( (-b) + Math.sqrt(discriminate))/(2*a);
+        let x2 = ( (-b) - Math.sqrt(discriminate))/(2*a);
+        result.push(x1,x2);
         return result;
     } 
     // код для задачи №1 писать здесь
