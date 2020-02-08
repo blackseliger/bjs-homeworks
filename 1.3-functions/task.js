c277155d323d72fdddcd9cb532374d644abfb3b2
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-02-08 18:59:10 +0300

    первое задание1.1

--- 1.3-functions/task.js
+++ 1.3-functions/task.js
@@ -16,46 +16,18 @@ function getSolutions(a, b, c) {
       return {
           D: D, roots: [x1,  x2]
       };
-  }
-  function showSolutionsMessage(a, b, c) {
-    let result = getSolutions(a, b, c);
+  }   
+}  
+
+function showSolutionsMessage(a, b, c) {
+    let result = getSolutions();
     console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
-    console.log(`Значение дискриминанта: ${result}`);
-    if (!roots.length) {
+    console.log(`Значение дискриминанта: ${D}`);
+    if (D < 0) {
         console.log(`Уравнение не имеет вещественных корней`);
-    } else if (roots.length == 1) {
+    } else if (D == 0) {
         console.log(`Уравнение имеет один корень Х1 = ${x1}`);
-    } else if (roots.length == 2){
+    } else if (D > 0){
         console.log(`Уравнение имеет два корня Х1 = ${х1}, X2 = ${x2}`);
     }
     }
-    // showSolutionsMessage();
-
-}  getSolutions();
-
-
-
-
-
-
-// function getResult(a,b,c){
-//     let discriminant; 
-//     discriminant = b**2 - 4*a*c;
-//     let result = [];
-//     if ( discriminant == 0) {
-//         // let result = [];
-//         let x1 = ( (-b))/(2*a);
-//         result.push(x1);
-//         // return result;
-//     } else if (discriminant < 0) {
-//         // let result = [];
-//         // return result;
-//     } else if ( discriminant > 0) {
-//         // let result = [];
-//         let x1 = ( (-b) + Math.sqrt(discriminant))/(2*a);
-//         let x2 = ( (-b) - Math.sqrt(discriminant))/(2*a);
-//         result.push(x1,x2);
-//         // return result;
-//     } 
-//     return result;
-// }
