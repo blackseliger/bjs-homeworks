08c9c37e1714c628d92c2253c2c7ae672511f8e4
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-02-08 19:30:08 +0300

    первое задание1.3

--- 1.3-functions/task.js
+++ 1.3-functions/task.js
@@ -1,20 +1,20 @@
 function getSolutions(a, b, c) {
-    let D = b**2 - 4*a*c;
-  if (D < 0) {
+    let d = b**2 - 4*a*c;
+  if (d < 0) {
       return {
-        D: D, roots: []  
+        D: d, roots: []  
       };
-  } else if ( D == 0 ) {
+  } else if ( d == 0 ) {
         let x1 = -b/2*a;   
     return {
-        D: D, roots: [x1]
+        D: d, roots: [x1]
       };
-  } else if ( D > 0) {
+  } else if ( d > 0) {
       let x1, x2;
-      x1 = ( (-b) + Math.sqrt(D))/(2*a);
-      x2 = ( (-b) - Math.sqrt(D))/(2*a);
+      x1 = ( (-b) + Math.sqrt(d))/(2*a);
+      x2 = ( (-b) - Math.sqrt(d))/(2*a);
       return {
-          D: D, roots: [x1,  x2]
+          D: d, roots: [x1,  x2]
       };
   }  
 }   
@@ -22,12 +22,12 @@ function getSolutions(a, b, c) {
     function showSolutionsMessage(a, b, c) {
     let result = getSolutions(a, b, c);
     console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
-    console.log(`Значение дискриминанта: ${D}`);
-    if (D < 0) {
+    console.log(`Значение дискриминанта: ${result.D}`);
+    if (result.D < 0) {
         console.log(`Уравнение не имеет вещественных корней`);
-    } else if (D == 0) {
-        console.log(`Уравнение имеет один корень Х1 = ${x1}`);
-    } else if (D > 0){
-        console.log(`Уравнение имеет два корня Х1 = ${х1}, X2 = ${x2}`);
+    } else if (result.D == 0) {
+        console.log(`Уравнение имеет один корень Х1 = ${result.roots[0]}`);
+    } else if (result.D > 0){
+        console.log(`Уравнение имеет два корня Х1 = ${result.roots[0]}, X2 = ${result.roots[1]}`);
     }
     }
