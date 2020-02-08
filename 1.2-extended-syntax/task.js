ed2e725dff4d40d81a852deeb38d92d76a8986db
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-02-08 14:31:41 +0300

    Исправленная работа 8 фер.

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,49 +1,47 @@
+"use strict"
+
 function getResult(a,b,c){
-    "use strict"
-    let discriminate; 
-    discriminate = b**2 - 4*a*c;
-    if ( discriminate == 0) {
-        let result = [];
+    let discriminant; 
+    discriminant = b**2 - 4*a*c;
+    let result = [];
+    if ( discriminant == 0) {
+        // let result = [];
         let x1 = ( (-b))/(2*a);
         result.push(x1);
-        return result;
-    } else if (discriminate < 0) {
-        let result = [];
-        return result;
-    } else if ( discriminate > 0) {
-        let result = [];
-        let x1 = ( (-b) + Math.sqrt(discriminate))/(2*a);
-        let x2 = ( (-b) - Math.sqrt(discriminate))/(2*a);
+        // return result;
+    } else if (discriminant < 0) {
+        // let result = [];
+        // return result;
+    } else if ( discriminant > 0) {
+        // let result = [];
+        let x1 = ( (-b) + Math.sqrt(discriminant))/(2*a);
+        let x2 = ( (-b) - Math.sqrt(discriminant))/(2*a);
         result.push(x1,x2);
-        return result;
+        // return result;
     } 
+    return result;
 }
 
 
 function getAverageMark(marks){
-    
+    let average;
     if (marks.length > 5) {
-            let sum = 0;
-            console.log("Превышено количество оценок");
+            console.log("Превышено количество оценок. Высчитывается только первые пять оценок");
             marks.splice(5, marks.length);
-            for ( let i = 0; i < marks.length; i++) {
-                sum = sum + marks[i];
-            } 
-            let average = sum/marks.length;
-            return average;
-        } else if (marks.length <= 5) {
+            } else if (!marks.length) {
+                average = 0;
+                return average;
+            }
             let sum = 0;
             for ( let i = 0; i < marks.length; i++) {
-                sum = sum + marks[i];     //  Почему, если тут написать sum =+ marks[i]; То команда не будет работать? 
-                
-            } 
-            let average = sum/marks.length;
-            return average; 
-        }
+            sum = sum + marks[i];
+    } 
+            average = sum/marks.length;
+            return average;
 }
 
 function askDrink(name,dateOfBirthday){
-   let todaydate = new Date();
+   let todaydate = new Date().getFullYear();
    let ageVerification = todaydate - dateOfBirthday;
    if ( ageVerification >= 18) {
        console.log(`Не желаете ли олд-фэшн, ${name}?`);
