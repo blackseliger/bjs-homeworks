3c0c75c25f240b35f730f8b53d5a1bf0c9d6122c
Laptop Gleb Bir <biryukov.gleb.98@mail.ru>
2020-02-17 20:50:59 +0300

    сделанная вся работа

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,41 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+
+    if ( animal === undefined) {
+        return null;
+    } else {
+        let sound = animal.sound;
+        return sound;
+    }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    
+    let sum = 0;
+    let i = 0;
+    for ( let prop in marks) {
+        if (+marks[prop]){
+            sum += +marks[prop];
+            i++
+        } else {
+            return 0;
+        }
+        }
+    // for ( let i = 0; i< marks.length; i++) {
+    //     sum += marks[i];
+    // } 
+    // let average = sum/marks.length;
+    let average = sum/i;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
+    let now = +date.now();
+    let calcMonth = new Date(birthday).getMonth;
+    let birthday = +date(birthday);
+    let diff = birthday - now;
+    let age = diff/now;
+    return (age > 18)? `Да`: `Нет`;
     // код для задачи №3 писать здесь
     // return verdict
 }
