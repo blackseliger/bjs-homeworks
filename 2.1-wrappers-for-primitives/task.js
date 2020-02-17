2416b8046c51eddce5ffaecd8249dcf79d08e945
Laptop Gleb Bir <biryukov.gleb.98@mail.ru>
2020-02-17 17:05:54 +0300

    сделанная вся работа до первой проверки

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,9 +1,38 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+    let checkArguments = {
+        percent,
+        contribution,
+        amount
+    } ;
+     for ( let prop in checkArguments) {
+         if (isNaN(checkArguments[prop])){
+             console.log(`Параметр ${prop} содержится нерпавильное значение ${checkArguments[prop]} `);
+         }
+     }
+     
+    let allMonths = new Date(date).getMonth() - new Date().getMonth() + ( 12 * (new Date(date).getFullYear() - new Date().getFullYear()));
+    let credit = amount - contribution;
+    let p = (percent/12)/100;
+    let paymentForMonth = credit*(p+p/((((1+p)**allMonths)-1)));
+    let totalAmount = allMonths * paymentForMonth;
+    console.log(totalAmount.toFixed(2));
+    return Number(totalAmount.toFixed(2));
+    
 }
 
 function getGreeting(name) {
+    function checkName(name) {
+        if ((name === undefined) || (name === "") || (name === null)) {
+            return 'Аноним';
+        } else {
+            return name
+        };
+    } 
+
+return (`Привет, мир! Меня зовут ${checkName(name)}`);
     // код для задачи №2 писать здесь
     // return greeting;
 }
