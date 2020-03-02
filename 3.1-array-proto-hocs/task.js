139ab5d829a2b871c09168906d966d09b22ad274
Laptop Gleb Bir <biryukov.gleb.98@mail.ru>
2020-03-02 04:39:59 +0300

    Intial data

--- 3.1-array-proto-hocs/task.js
+++ 3.1-array-proto-hocs/task.js
@@ -13,15 +13,57 @@ function sleep(milliseconds) {
     }, 0);
   }
 
- let compareArrays = function( arr1, arr2) {
-     return ( arr1.length === arr2.length && 
-        arr1.every === arr2.every )
-
- } 
+const compareArrays = function(arr1, arr2) {
+   return (
+     arr1.length === arr2.length &&
+     arr1.every(function(el, i) {
+      //   debugger;
+      // console.log(el, i);
+       return el === arr2[i];
+     })
+   );
+ };
 
  function memorize (fn , limit) {
+   let results = [];
+   let i = 0;  // для замыкания 
+    return function(){
+      let arg = Array.from(arguments);  // массив аргументов возвращаемой функции
+      let findObj = results.find(x => compareArrays(x.arg, args)); // x это обозначение свойства массива
+      console.log(`Было сделано ${i++} вызовов `)
+      // results.push({arg: args, result: fn })
+      if (results.find === undefined ) {
          
-    return limit = fn
+         let calculation = fn.apply(null, args ); // как вызвать функцию? Это нормальный ход действий?
+         console.log(`функция ${calculation} вызвана не из памяти.`)
+         results.push({args: arg, result: calculation})
+         if (result.length > limit) {
+            result.shift();
+         }
+      } else {
+         let calculation = findObj.result;
+         console.log(`функция ${calculation} вызвана из памяти`)
+      }
+      return calculation;
+    }
  }
 
- const mSum = memorize(sum, 5);
+ function testCase(testFunction, timer){
+   let arrays = [ 
+      [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] 
+   ]
+
+   console.time(timer);
+   for ( let i = 0; i < 100; i++){
+      let calledFunc = testFunction.apply(null, arguments);
+      calledFunc.forEach(element => {
+         return element * i;
+      }); 
+         };
+   console.timeEnd(timer)
+   }
+ 
+   testCase(sum, 100) // какой таймер ставить?
+
+
+
